8ec7617c31bce48a042723b4e04211a16a9cdd26
Alisson <alisson.souz.pa@gmail.com>
2022-03-30 16:24:43 -0300

    flags details

--- src/Components/CountryDetails.jsx
+++ src/Components/CountryDetails.jsx
@@ -1,44 +1,77 @@
 import React from 'react'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import  { useParams, useNavigate } from 'react-router'
 import '../App.css'
 
-function CountryDetails({darkMode}) {
+function CountryDetails({darkMode, countries}) {
+    const params = useParams();
+    const navigate = useNavigate();
+
+    let name;
+    let flagImg;
+    let nativeName;
+    let population;
+    let region;
+    let subregion;
+    let capital;
+    let topLevalDomain;
+    let currencies = [];
+    let languages = [];
+    let borders = [];
+
+    countries.forEach(country => {
+        if(country.alpha3Code === params.countryCode){
+            name = country.name;
+            flagImg = country.flag;
+            nativeName = country.nativeName;
+            population = country.population;
+            region = country.region;
+            subregion = country.subregion;
+            capital = country.capital;
+            topLevalDomain = country.topLevalDomain;
+        }
+    })
+
+    const goBack =() => {
+        navigate("/");
+    }
+    
     return (
         <div className="country-details" >
-            <buttom className={`back ${darkMode ? 'darkMode' : ''}`}>
+            <buttom className={`back ${darkMode ? 'darkMode' : ''}`} onClick={goBack}>
                 <ArrowBackIcon />
                 <p>Go Back</p>
             </buttom>
 
             <div className="country-details-body">
                 <div className="img-container">
-                    <img src="" alt="" />
+                    <img src={flagImg} alt="" />
                 </div>
 
                 <div className="info">
-                    <h2>Name</h2>
+                    <h2>{name}</h2>
                     <div className="info-container">
                         <div className="left-info">
                             <p>
-                                Native Name: <span className={`values ${darkMode ? 'darkMode' : ''}`}>Test</span>
+                                Native Name: <span className={`values ${darkMode ? 'darkMode' : ''}`}>{nativeName}</span>
                             </p>
                             <p>
-                                Population: <span className={`values ${darkMode ? 'darkMode' : ''}`}>Test</span>
+                                Population: <span className={`values ${darkMode ? 'darkMode' : ''}`}>{population}</span>
                             </p>
                             <p>
-                                Region: <span className={`values ${darkMode ? 'darkMode' : ''}`}>Test</span>
+                                Region: <span className={`values ${darkMode ? 'darkMode' : ''}`}>{region}</span>
                             </p>
                             <p>
-                                Sub Region: <span className={`values ${darkMode ? 'darkMode' : ''}`}>Test</span>
+                                Sub Region: <span className={`values ${darkMode ? 'darkMode' : ''}`}>{subregion}</span>
                             </p>
 
                         </div>
                         <div className="right-info">
                             <p>
-                                Capital: <span className={`values ${darkMode ? 'darkMode' : ''}`}>Test</span>
+                                Capital: <span className={`values ${darkMode ? 'darkMode' : ''}`}>{capital}</span>
                             </p>
                             <p>
-                                Top-level Domain: <span className={`values ${darkMode ? 'darkMode' : ''}`}>Test</span>
+                                Top-level Domain: <span className={`values ${darkMode ? 'darkMode' : ''}`}>{topLevalDomain}</span>
                             </p>
                             <p>
                                 Currencies: <span className={`values ${darkMode ? 'darkMode' : ''}`}>Test</span>
